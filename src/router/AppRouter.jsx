43a9269d55b82c2091fde4f8b0682a8185ee5c92
gejaga12 <gejaga12@hotmail.com>
2023-03-30 11:08:33 -0300

    0.1

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,7 +4,7 @@ import { CalendarPage } from "../calendar";
 
 export const AppRouter = () => {
 
-  const authStatus = "not-authenticated" // "authenticated"; 
+  const authStatus = "not-authenticated" // "authenticatedd"; 
 
   return (
     <Routes>
