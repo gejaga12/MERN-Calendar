67fee8cf29000938f7a0102caace246c32c59961
gejaga12 <gejaga12@hotmail.com>
2023-03-30 13:30:08 -0300

    calendar funcionando

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,5 +1,47 @@
-import React from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+import { NavBar } from "../";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "",
+    notes: "",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "1",
+      name: "Gerardo",
+    },
+  },
+];
 
 export const CalendarPage = () => {
-  return <div>CalendarPage</div>;
+  return (
+    <>
+      <NavBar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </>
+  );
 };
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,7 +4,7 @@ import { CalendarPage } from "../calendar";
 
 export const AppRouter = () => {
 
-  const authStatus = "not-authenticated" // "authenticateddd"; 
+  const authStatus = "authenticated" // "authenticateddd"; 
 
   return (
     <Routes>
